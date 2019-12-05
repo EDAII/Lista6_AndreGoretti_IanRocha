0780f80ec247c01f7cd4cc11143785fae51b23d8
Agoretti <andremotta96@gmail.com>
2019-12-05 17:34:30 -0300

    arrmando caminho vazio

--- grafo.py
+++ grafo.py
@@ -27,43 +27,24 @@ def caminhosDFS(grafo, start, goal):
             else:
                 pilha.append((vizinho, aresta + [vizinho]))
 
-
-# finds shortest path between 2 nodes of a graph using BFS
-def bfs_shortest_path(graph, start, goal):
-    # keep track of explored nodes
-    explored = []
-    # keep track of all the paths to be checked
-    queue = [[start]]
- 
-    # return path if start is goal
-    if start == goal:
-        return "That was easy! Start = goal"
- 
-    # keeps looping until all possible paths have been checked
+def bfs_paths(graph, start, goal):
+    queue = [(start, [start])]
     while queue:
-        # pop the first path from the queue
-        path = queue.pop(0)
-        # get the last node from the path
-        node = path[-1]
-        if node not in explored:
-            neighbours = graph[node]
-            # go through all neighbour nodes, construct a new path and
-            # push it into the queue
-            for neighbour in neighbours:
-                new_path = list(path)
-                new_path.append(neighbour)
-                queue.append(new_path)
-                # return path if neighbour is goal
-                if neighbour == goal:
-                    return new_path
- 
-            # mark node as explored
-            explored.append(node)
- 
-    # in case there's no path between the 2 nodes
-    return "So sorry, but a connecting path doesn't exist :("
- 
+        (vertex, path) = queue.pop(0)
+        for next in graph[vertex] - set(path):
+            if next == goal:
+                yield path + [next]
+            else:
+                queue.append((next, path + [next]))
 
+def shortest_path(graph, start, goal):
+    try:
+        return next(bfs_paths(graph, start, goal))
+    except StopIteration:
+        return []          
+
+
+ 
 
 
 
@@ -81,15 +62,20 @@ grafo = {'A': set([random.choice(listaA), random.choice(listaA)]),
          }
 
 # shortest_path(grafo, 'A', 'F')
-caminhos = list(bfs_shortest_path(grafo, 'A', 'F'))
+temp = list(shortest_path(grafo, 'A', 'F'))
+if temp is None:
+    caminhos = []
+else:   
+    caminhos = temp
+    # caminhos = list(shortest_path(grafo, 'A', 'F'))
 
 
-numero = 40
-caminho_menor = []
-for x in caminhos:
-    if len(x) < numero:
-        caminho_menor = x
-        numero = len(x)
+# numero = 40
+# caminho_menor = []
+# for x in caminhos:
+#     if len(x) < numero:
+#         caminho_menor = x
+#         numero = len(x)
 
 print(caminhos)
 # print(caminho_menor)
